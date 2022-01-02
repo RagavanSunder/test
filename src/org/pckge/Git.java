147d7cfc2d969fcc9527b8362309fb242743f042
RagavanSunder <ragavan16795@gmail.com>
2022-01-02 13:26:24 +0530

    Me3 task

--- src/org/pckge/Git.java
+++ src/org/pckge/Git.java
@@ -11,7 +11,10 @@ e.add();
 public void sub() {
 System.out.println("sub");
 }
-public void mul() {
-System.out.println("mul");
+public void divi() {
+System.out.println("divi");
+}
+public void vivi() {
+System.out.println("vivi");
 }
 }
